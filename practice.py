b79587903585d2f8368329f62aee6cd68eea93a4
abdul-wahab94 <wahabsiddique007@gmail.com>
2026-02-06 03:54:23 +0500

    Assignment 1

---
+++ practice.py
@@ -0,0 +1,18 @@
+def question1():
+    customer_ids = ["C101" , "C102" , "C103" , "C104"]
+    n=0
+    for var in range(1,len(customer_ids)+1) :
+        print(customer_ids[n])
+        n=n+1
+    print("Program End")
+
+question1()
+
+
+def question2():
+    customer_ids = ["C101" , "C102" , "C103" , "C104"]
+    for num in customer_ids:
+        print(f" Dear {num}, Your Data Has Been Processed Successfully ")
+
+question2()
+
